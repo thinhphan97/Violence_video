3e73fd1b260f046de9024d3605cfd3962256d012
thinhphan97 <thinhphan130997@gmail.com>
2021-10-12 09:53:07 +0700

    update file train.py

--- train.py
+++ train.py
@@ -1,5 +1,11 @@
 import argparse
-
+from src.utils import *
+from tqdm import tqdm
+import apex
+from apex import amp
+import torch
+import torch.nn as nn
+from sklearn.metrics import roc_auc_score, log_loss
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -30,8 +36,90 @@ def build_model(cfg):
 def test_model(_print, cfg, model, test_loader):
     pass
 def valid_model(_print, cfg, model, valid_loader, valid_criterion):
-    pass
+    model.eval()
+
+    preds = []
+    targets = []
+    tbar = tqdm(valid_loader)
+    with torch.no_grad():
+        for i, (image, target) in enumerate(tbar):
+            image = image.cuda()
+            target = target.cuda()
+            bsize, seq_len, c, h, w = image.size()
+            image = image.view(bsize * seq_len, c, h, w)
+            target = target.view(-1, target.size(-1))
+            output = model(image, seq_len)
+            preds.append(output.cpu())
+            targets.append(target.cpu())
+
+    preds, targets = torch.cat(preds, 0), torch.cat(targets, 0)
+    # record loss
+    loss_tensor = valid_criterion(preds, targets)
+    val_loss = loss_tensor.sum() / valid_criterion.class_weights.sum()
+    any_loss = loss_tensor[0]
+    intraparenchymal_loss = loss_tensor[1]
+    intraventricular_loss = loss_tensor[2]
+    subarachnoid_loss = loss_tensor[3]
+    subdural_loss = loss_tensor[4]
+    epidural_loss = loss_tensor[5]
+    _print("Val. loss: %.5f - any: %.3f - intraparenchymal: %.3f - intraventricular: %.3f - subarachnoid: %.3f - subdural: %.3f - epidural: %.3f" % (
+        val_loss, any_loss,
+        intraparenchymal_loss, intraventricular_loss,
+        subarachnoid_loss, subdural_loss, epidural_loss))
+    # record AUC
+    auc = roc_auc_score(targets[:, 1:].numpy(), preds[:, 1:].numpy(), average=None)
+    _print("Val. AUC - intraparenchymal: %.3f - intraventricular: %.3f - subarachnoid: %.3f - subdural: %.3f - epidural: %.3f" % (
+            auc[0], auc[1], auc[2], auc[3], auc[4]))
+    return val_loss
+
 def train_loop(_print, cfg, model, train_loader, criterion, valid_loader, valid_criterion, optimizer, scheduler, start_epoch, best_metric):
-    pass
+    for epoch in range(start_epoch, cfg.TRAIN.EPOCHS):
+        _print(f"\nEpoch {epoch + 1}")
+
+        losses = AverageMeter()
+        model.train()
+        tbar = tqdm(train_loader)
+
+        for i, (image, target) in enumerate(tbar):
+            image = image.cuda()
+            target = target.cuda()
+            bsize, seq_len, c, h, w = image.size()
+            image = image.view(bsize * seq_len, c, h, w)
+            target = target.view(-1, target.size(-1))
+
+            output = model(image, seq_len)
+            
+            # gradient accumulation
+            loss = loss / cfg.OPT.GD_STEPS
+
+            if cfg.SYSTEM.FP16:
+                with amp.scale_loss(loss, optimizer) as scaled_loss:
+                    scaled_loss.backward()
+            else:
+                loss.backward()
+
+            if (i + 1) % cfg.OPT.GD_STEPS == 0:
+                optimizer.step()
+                scheduler.step()
+                optimizer.zero_grad()
+
+            # record loss
+            losses.update(loss.item() * cfg.OPT.GD_STEPS, image.size(0))
+            tbar.set_description("Train loss: %.5f, learning rate: %.6f" % (losses.avg, optimizer.param_groups[-1]['lr']))
+
+        _print("Train loss: %.5f, learning rate: %.6f" % (losses.avg, optimizer.param_groups[-1]['lr']))
+
+        loss = valid_model(_print, cfg, model, valid_loader, valid_criterion)
+        is_best = loss < best_metric
+        best_metric = min(loss, best_metric)
+
+        save_checkpoint({
+            "epoch": epoch + 1,
+            "arch": cfg.EXP,
+            "state_dict": model.state_dict(),
+            "best_metric": best_metric,
+            "optimizer": optimizer.state_dict(),
+        }, is_best, root=cfg.DIRS.WEIGHTS, filename=f"{cfg.EXP}.pth")
+
 def main(args, cfg):
     pass
